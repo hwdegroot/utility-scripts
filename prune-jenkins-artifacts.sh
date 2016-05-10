c28ebbd029f5238b3e7c0a93227571ef7a6d2d6b
Rik de Groot <rik.de.groot@mendix.com>
2016-05-10 23:03:29 +0200

    Tweak jenkis pruning

--- prune-jenkins-artifacts.sh
+++ prune-jenkins-artifacts.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-JENKINS_HOME="${JENKINS_HOME:-$1}"
+JENKINS_HOME="${JENKINS_HOME:-"$1"}"
 
-find "$JENKINS_HOME/jobs" -mtime +7 -path "*/builds/[0-9]*/artifacts" -print0 | xargs -0i rm -rf {}
+if [[ ! -d "$JENKINS_HOME" ]]; then
+	>&2 echo "JENKINS_HOME is not set."
+	exit 1
+fi
+
+
+tmpfile=$(mktemp)
+find "$JENKINS_HOME/jobs" -mtime +10 -path "*/builds/[0-9]*/artifacts" > "$tmpfile"
+
+while read line; do
+	if [[ -z "$line" || ! -d "$line"  || "$line" == "." || "$line" == ".." || "$line" == "/" ]]; then
+		continue
+	fi
+	rm -rf "$line"
+done < <(cat "$tmpfile")
